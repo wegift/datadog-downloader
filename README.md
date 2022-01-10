4ceb47c6b2af1521955bdf1d4efbfec683143b08
Rob Murphy <rob.murphy@wegift.io>
2022-01-10 20:21:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# DataDog log downloader
+
+Download logs from DataDog via the API.
+This allows you to download a large number of logs matching a particular query rather than being bound by the 5000 limit imposed on the export button in the UI.
+Logs are output as a JSON file.
+
+## Usage
+
+```
+DD_API_KEY=... DD_APP_KEY=... npx github:wegift/datadog-downloader --query '"Redeem failed"'
+```
+
+## Authentication
+
+You will need an API key and an app key to access the DataDog api.
+These should be provided in environment variables as seen above.
+
+API keys are global for a DataDog account and can be found in organization settings.
+App keys are personal to your profile and can be generated in personal settings.
+
+## Options
+
+```
+--query     The filter query (aka search term). Take care when quoting on the command line, single quote the entire query for best results.
+
+--index     Which index to read from, default 'main'
+
+--from      Start date/time defaults to 1y ago
+--to        End date/time, omit for results up to the current time
+
+--pageSize  How many results to download at a time, default 1000 limit of 5000
+
+--output    Path of json file to write results to, default 'results.json'
+```
+
+Note: Date/times are parsed by JS `Date` constructor, e.g. 2022-01-01
+
+## Local Dev
+
+Run `npm install`.
+
+Copy `.env.example` to `.env` and add a valid DataDog API key and app key.
+
+### Run
+
+```
+node index.mjs --query '"Redeem token failure" -@redeem_failure_reason:"Invalid token"'
+```
+
+## Caveats
+
+Logs are not streamed, they are all stored in memory and stringified / written as as single action.
+I have tested with 25k logs and there were no issues, the resultant JSON file was only 100mb so it seems likely that
+you could download 100k+ without running into memory or performance limits but ymmv.
+
+
+## Contributing
+
+**Pull requests welcome!**
---
+++ index.mjs
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+import { v2 } from "@datadog/datadog-api-client";
+import chalk from "chalk";
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+import yargs from "yargs";
+const argv = yargs(process.argv).argv;
+
+dotenv.config();
+
+const configuration = v2.createConfiguration();
+const apiInstance = new v2.LogsApi(configuration);
+
+async function getLogs(apiInstance, params) {
+    const data = [];
+
+    let nextPage = null;
+    let n = 0;
+    do {
+        console.log(`Requesting page ${++n} ${nextPage ? `with cursor ${nextPage} ` : ``}`);
+        const query = nextPage ? { ...params, pageCursor: nextPage } : params;
+        const result = await apiInstance.listLogsGet(query);
+        data.push(...result.data);
+        nextPage = result?.meta?.page?.after;
+        console.log(`${result.data.length} results (${data.length} total)`);
+    } while (nextPage);
+
+    return data;
+}
+
+function oneYearAgo() {
+    return new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+}
+
+const initialParams = {
+    filterQuery: argv.query,
+    filterIndex: argv.index ?? "main",
+    filterFrom: argv.from ? new Date(argv.from) : oneYearAgo(),
+    filterTo: argv.top ? new Date(argv.to) : new Date(),
+    pageLimit: argv.pageSize ? Math.min(argv.pageSize, 5000) : 1000,
+};
+
+if (!initialParams.filterQuery) {
+    console.log(chalk.red("Error: No query supplied, use --query"));
+    process.exit();
+}
+
+console.log(chalk.cyan("Downloading logs:\n" + JSON.stringify(initialParams, null, 2) + "\n"));
+
+(async function () {
+    let data;
+    try {
+        data = await getLogs(apiInstance, initialParams);
+    } catch (e) {
+        console.log(chalk.red(e.message));
+        process.exit(1);
+    }
+
+    const outputFile = argv.output ?? "results.json";
+    console.log(chalk.cyan(`\nWriting ${data.length} logs to ${outputFile}`));
+    fs.writeFileSync(outputFile, JSON.stringify(data, null, 2));
+
+    console.log(chalk.green("Done!"));
+})();
