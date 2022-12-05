c77693fa9938a00fa2f5f87225f62450f8b2472a
Koen De Groote <kdg.dev@gmail.com>
2022-12-05 17:39:44 +0100

    Updated libraries. Applied prettier

--- index.mjs
+++ index.mjs
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
 
-import { v2 } from "@datadog/datadog-api-client";
-import chalk from "chalk";
-import * as dotenv from "dotenv";
-import * as fs from "fs";
-import yargs from "yargs";
+import { client, v2 } from '@datadog/datadog-api-client';
+import chalk from 'chalk';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import yargs from 'yargs';
 const argv = yargs(process.argv).argv;
 
-dotenv.config();
+const key_config = dotenv.config();
 
-const configuration = v2.createConfiguration();
+const configurationOpts = {
+    authMethods: {
+        apiKeyAuth: key_config.parsed.DD_API_KEY,
+        appKeyAuth: key_config.parsed.DD_APPLICATION_KEY,
+    },
+};
+
+const configuration = client.createConfiguration(configurationOpts);
+
+client.setServerVariables(configuration, {
+    site: key_config.parsed.DD_SITE,
+});
+
 const apiInstance = new v2.LogsApi(configuration);
 
 async function getLogs(apiInstance, params) {
@@ -35,18 +47,18 @@ function oneYearAgo() {
 
 const initialParams = {
     filterQuery: argv.query,
-    filterIndex: argv.index ?? "main",
+    filterIndex: argv.index ?? 'main',
     filterFrom: argv.from ? new Date(argv.from) : oneYearAgo(),
     filterTo: argv.to ? new Date(argv.to) : new Date(),
     pageLimit: argv.pageSize ? Math.min(argv.pageSize, 5000) : 1000,
 };
 
 if (!initialParams.filterQuery) {
-    console.log(chalk.red("Error: No query supplied, use --query"));
+    console.log(chalk.red('Error: No query supplied, use --query'));
     process.exit();
 }
 
-console.log(chalk.cyan("Downloading logs:\n" + JSON.stringify(initialParams, null, 2) + "\n"));
+console.log(chalk.cyan('Downloading logs:\n' + JSON.stringify(initialParams, null, 2) + '\n'));
 
 (async function () {
     let data;
@@ -57,9 +69,10 @@ console.log(chalk.cyan("Downloading logs:\n" + JSON.stringify(initialParams, nul
         process.exit(1);
     }
 
-    const outputFile = argv.output ?? "results.json";
+    const outputFile = argv.output ?? 'results.json';
     console.log(chalk.cyan(`\nWriting ${data.length} logs to ${outputFile}`));
     fs.writeFileSync(outputFile, JSON.stringify(data, null, 2));
 
-    console.log(chalk.green("Done!"));
+    console.log(chalk.green('Done!'));
 })();
+
